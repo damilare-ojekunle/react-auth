11b79d6e773ea6e3651661187d6c10cb801efb02
Dev- Darey <ojeksdarey@gmail.com>
2022-06-25 12:52:44 +0100

    frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,48 +1,28 @@
-import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
-import { Link, useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { logout, reset } from '../features/auth/authSlice'
+import { FaUser, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-function Header() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { user } = useSelector((state) => state.auth)
-
-  const onLogout = () => {
-    dispatch(logout())
-    dispatch(reset())
-    navigate('/')
-  }
-
+function Header(props) {
   return (
-    <header className='header'>
-      <div className='logo'>
-        <Link to='/'>GoalSetter</Link>
-      </div>
-      <ul>
-        {user ? (
+    <div>
+      <header className="header">
+        <div className="logo">
+          <Link to="/">GoalSetter</Link>
+        </div>
+        <ul>
           <li>
-            <button className='btn' onClick={onLogout}>
-              <FaSignOutAlt /> Logout
-            </button>
+            <Link to="/login">
+              <FaSignInAlt /> Login
+            </Link>
           </li>
-        ) : (
-          <>
-            <li>
-              <Link to='/login'>
-                <FaSignInAlt /> Login
-              </Link>
-            </li>
-            <li>
-              <Link to='/register'>
-                <FaUser /> Register
-              </Link>
-            </li>
-          </>
-        )}
-      </ul>
-    </header>
-  )
+          <li>
+            <Link to="/register">
+              <FaUser /> Register
+            </Link>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,103 +1,63 @@
-import { useState, useEffect } from 'react'
-import { FaSignInAlt } from 'react-icons/fa'
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import { login, reset } from '../features/auth/authSlice'
-import Spinner from '../components/Spinner'
-
-function Login() {
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+function Login(props) {
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  })
-
-  const { email, password } = formData
-
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
-  const { user, isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.auth
-  )
-
-  useEffect(() => {
-    if (isError) {
-      toast.error(message)
-    }
-
-    if (isSuccess || user) {
-      navigate('/')
-    }
-
-    dispatch(reset())
-  }, [user, isError, isSuccess, message, navigate, dispatch])
-
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
-    }))
-  }
-
+    }));
+  };
   const onSubmit = (e) => {
-    e.preventDefault()
-
-    const userData = {
-      email,
-      password,
-    }
-
-    dispatch(login(userData))
-  }
-
-  if (isLoading) {
-    return <Spinner />
-  }
-
+    e.preventDefault();
+  };
   return (
     <>
-      <section className='heading'>
+      <section className="heading">
         <h1>
-          <FaSignInAlt /> Login
+          <FaSignInAlt />
+          Login
         </h1>
-        <p>Login and start setting goals</p>
+        <p>Login and start setting goals </p>
       </section>
-
-      <section className='form'>
+      <section className="form">
         <form onSubmit={onSubmit}>
-          <div className='form-group'>
+          <div className="form-group">
             <input
-              type='email'
-              className='form-control'
-              id='email'
-              name='email'
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
               value={email}
-              placeholder='Enter your email'
+              placeholder="Enter your email"
               onChange={onChange}
             />
           </div>
-          <div className='form-group'>
+          <div className="form-group">
             <input
-              type='password'
-              className='form-control'
-              id='password'
-              name='password'
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
               value={password}
-              placeholder='Enter password'
+              placeholder="Enter password"
               onChange={onChange}
             />
           </div>
 
-          <div className='form-group'>
-            <button type='submit' className='btn btn-block'>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
               Submit
             </button>
           </div>
         </form>
       </section>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
